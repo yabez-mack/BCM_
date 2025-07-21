2eef024cd15d2d47ff42f19d20c66273b0c7cbfe
yabez <mackyabez@gmail.com>
2025-07-21 12:16:02 +0530

    -yabez

--- src/app/components/field_report/field_report.component.ts
+++ src/app/components/field_report/field_report.component.ts
@@ -15,11 +15,14 @@ import { QuillEditorComponent } from 'ngx-quill';
   templateUrl: './field_report.component.html',
   styleUrls: ['./field_report.component.css'],
 })
-export class FieldReportComponent implements OnInit , AfterViewInit {
-  @ViewChild('obstacles_editor', { static: false }) quillEditorComponent1!: QuillEditorComponent;
-  @ViewChild('testimony_editor', { static: false }) quillEditorComponent2!: QuillEditorComponent;
-  @ViewChild('prayer_editor', { static: false }) quillEditorComponent3!: QuillEditorComponent;
-ngAfterViewInit(): void {
+export class FieldReportComponent implements OnInit, AfterViewInit {
+  @ViewChild('obstacles_editor', { static: false })
+  quillEditorComponent1!: QuillEditorComponent;
+  @ViewChild('testimony_editor', { static: false })
+  quillEditorComponent2!: QuillEditorComponent;
+  @ViewChild('prayer_editor', { static: false })
+  quillEditorComponent3!: QuillEditorComponent;
+  ngAfterViewInit(): void {
     const editor1 = this.quillEditorComponent1.quillEditor;
     const editor2 = this.quillEditorComponent2.quillEditor;
     const editor3 = this.quillEditorComponent3.quillEditor;
@@ -37,9 +40,7 @@ ngAfterViewInit(): void {
     private router: Router,
     private navbar: NavbarComponent,
     private quillConfig: QuillConfigService
-
-  ) {
-  }
+  ) {}
   field_report_form = this._fb.group({
     employee_id: [''],
     year: [''],
@@ -50,13 +51,13 @@ ngAfterViewInit(): void {
     obstacles: [''],
     status: ['1'],
   });
-  
+
   field_form = this._fb.group({
     branch_id: [''],
     field_name: [''],
-    status: ['1'],   
+    status: ['1'],
   });
-  searchReport:any=''
+  searchReport: any = '';
   submit_dashboard_casrol() {
     let body = {
       // title: this.dashboard_casrol_form.value.title,
@@ -68,12 +69,12 @@ ngAfterViewInit(): void {
     };
     if (!body.token) {
       Swal.fire({ title: 'Please Login', icon: 'info' });
-    } 
+    }
     // else if (!body.title) {
     //   Swal.fire({ title: 'Please Enter Title', icon: 'info' });
     // } else if (!body.image_url) {
     //   Swal.fire({ title: 'Please Enter URL', icon: 'info' });
-    // } 
+    // }
     else {
       this._auth.submit_dashboard_images(body).subscribe((res: any) => {
         if (res.status == 'success') {
@@ -92,24 +93,24 @@ ngAfterViewInit(): void {
     }
   }
   getUsedFonts(delta: any): string[] {
-  const fonts = new Set<string>();
+    const fonts = new Set<string>();
 
-  delta.ops.forEach((op:any) => {
-    if (op.attributes?.font) {
-      fonts.add(op.attributes.font);
-    }
-  });
+    delta.ops.forEach((op: any) => {
+      if (op.attributes?.font) {
+        fonts.add(op.attributes.font);
+      }
+    });
 
-  return Array.from(fonts);
-}
+    return Array.from(fonts);
+  }
   submit_field() {
     // Get Delta (recommended for preserving formats)
     const delta1 = this.quillEditorComponent1.quillEditor.getContents();
     const delta2 = this.quillEditorComponent2.quillEditor.getContents();
     const delta3 = this.quillEditorComponent3.quillEditor.getContents();
-    let font1=this.getUsedFonts(delta1)
-    let font2=this.getUsedFonts(delta2)
-    let font3=this.getUsedFonts(delta3)
+    let font1 = this.getUsedFonts(delta1);
+    let font2 = this.getUsedFonts(delta2);
+    let font3 = this.getUsedFonts(delta3);
 
     let body = {
       employee_id: this.field_report_form.value.employee_id,
@@ -120,25 +121,21 @@ ngAfterViewInit(): void {
       testimony: this.field_report_form.value.testimony,
       prayer_request: this.field_report_form.value.prayer_request,
       obstacles: this.field_report_form.value.obstacles,
-      obstacle_font:font1[0] ,
-      testimony_font:font2[0] ,
-      prayer_request_font:font3[0] ,
+      obstacle_font: font1[0],
+      testimony_font: font2[0],
+      prayer_request_font: font3[0],
       token: this.token,
     };
-    console.log(body)
+    console.log(body);
     if (!body.token) {
       Swal.fire({ title: 'Please Login', icon: 'info' });
-    } 
-     else if (!body.employee_id) {
+    } else if (!body.employee_id) {
       Swal.fire({ title: 'Please Select Employee', icon: 'info' });
-    }
-     else if (!body.year) {
+    } else if (!body.year) {
       Swal.fire({ title: 'Please Select Year', icon: 'info' });
-    }
-     else if (!body.month) {
+    } else if (!body.month) {
       Swal.fire({ title: 'Please Select Month', icon: 'info' });
-    }
-     else {
+    } else {
       this._auth.add_field_report(body).subscribe((res: any) => {
         if (res.status == 'success') {
           Swal.fire({ title: 'Submitted Successfully', icon: 'success' });
@@ -148,101 +145,84 @@ ngAfterViewInit(): void {
             prayer_request: '',
             testimony: '',
             year: '',
-            new_followers:'',
-            obstacles:'',
-            status:'1'
-            
+            new_followers: '',
+            obstacles: '',
+            status: '1',
           });
-          this.get_field_report()
+          this.get_field_report();
         } else {
           Swal.fire({ title: res.message, icon: 'error' });
         }
       });
     }
   }
-  
+
   token: any = '';
   user_id: any = '';
-  employees_list:any[]=[]
-  report_list:any[]=[]
-  get_employees(){
-    let body={
-      token:this.service.get('token')
-    }
-    this._auth.get_employees(body).subscribe((res:any)=>{
-      if(res.status=='success'){
-        this.employees_list=res.data
+  employees_list: any[] = [];
+  report_list: any[] = [];
+  get_employees() {
+    let body = {
+      token: this.service.get('token'),
+    };
+    this._auth.get_employees(body).subscribe((res: any) => {
+      if (res.status == 'success') {
+        this.employees_list = res.data;
       }
-    })
-
+    });
   }
-  get_field_report(){
-    let body={
-      token:this.service.get('token')
-    }
-    this._auth.get_field_report(body).subscribe((res:any)=>{
-      if(res.status=='success'){
-        this.report_list=res.data
-
+  get_field_report() {
+    let body = {
+      token: this.service.get('token'),
+    };
+    this._auth.get_field_report(body).subscribe((res: any) => {
+      if (res.status == 'success') {
+        this.report_list = res.data;
       }
-    })
-
+    });
   }
-  conv_month(item:any){
-    if(item==0){
-      return 'Jan'
-    }
-    else if(item==1){
-      return 'Feb'
-    }
-    else if(item==2){
-      return 'Mar'
-    }
-    else if(item==3){
-      return 'Apr'
-    }
-    else if(item==4){
-      return 'May'
-    }
-    else if(item==5){
-      return 'June'
-    }
-    else if(item==6){
-      return 'July'
-    }
-    else if(item==7){
-      return 'Aug'
-    }
-    else if(item==8){
-      return 'Sep'
-    }
-    else if(item==9){
-      return 'Oct'
-    }
-    else if(item==10){
-      return 'Nov'
-    }
-    else if(item==11){
-      return 'Dec'
-    }
-    else{
-      return '-'
+  conv_month(item: any) {
+    if (item == 0) {
+      return 'Jan';
+    } else if (item == 1) {
+      return 'Feb';
+    } else if (item == 2) {
+      return 'Mar';
+    } else if (item == 3) {
+      return 'Apr';
+    } else if (item == 4) {
+      return 'May';
+    } else if (item == 5) {
+      return 'June';
+    } else if (item == 6) {
+      return 'July';
+    } else if (item == 7) {
+      return 'Aug';
+    } else if (item == 8) {
+      return 'Sep';
+    } else if (item == 9) {
+      return 'Oct';
+    } else if (item == 10) {
+      return 'Nov';
+    } else if (item == 11) {
+      return 'Dec';
+    } else {
+      return '-';
     }
   }
-  year_list:any[]=[]
-  get_year(){
-    let body={
-      token:this.service.get('token')
-    }
-    this._auth.get_year(body).subscribe((res:any)=>{
-      if(res.status=='success'){
-        this.year_list=res.data
+  year_list: any[] = [];
+  get_year() {
+    let body = {
+      token: this.service.get('token'),
+    };
+    this._auth.get_year(body).subscribe((res: any) => {
+      if (res.status == 'success') {
+        this.year_list = res.data;
       }
-    })
-
+    });
   }
-  text:any
-  tools:any = {
+  text: any;
+  tools: any = {
     // imageResize: {},
     // syntax: {
     //   // highlight: (text:any) => hljs.highlightAuto(text).value,
@@ -250,26 +230,25 @@ ngAfterViewInit(): void {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],
       ['blockquote', 'code-block'],
-      [{ 'font': ['sans-serif','monospace',, 'Bamini'] }],
-      [{ header: 1 }, { header: 2 }], 
+      [{ font: ['sans-serif', 'monospace', , 'Bamini'] }],
+      [{ header: 1 }, { header: 2 }],
       [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }], 
-      [{ indent: '-1' }, { indent: '+1' }], 
-      [{ direction: 'rtl' }], 
-      [{ size: ['small', false, 'large', 'huge'] }], 
+      [{ script: 'sub' }, { script: 'super' }],
+      [{ indent: '-1' }, { indent: '+1' }],
+      [{ direction: 'rtl' }],
+      [{ size: ['small', false, 'large', 'huge'] }],
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ color: [] }, { background: [] }], 
-      
+      [{ color: [] }, { background: [] }],
+
       [{ align: [] }],
-      ['clean'], 
-      ['link', 'image', 'video'], 
+      ['clean'],
+      ['link', 'image', 'video'],
     ],
   };
-  
- 
+
   downloadPDF(): void {
     const doc = new jsPDF();
-    // const doc = new jsPDF('p', 'mm', 'a4'); 
+    // const doc = new jsPDF('p', 'mm', 'a4');
     // // 'p' for portrait, 'mm' for millimeters, 'a4' for A4 size
     const element = document.getElementById('contentToExport');
 
@@ -277,22 +256,22 @@ ngAfterViewInit(): void {
       doc.html(element, {
         callback: function (doc) {
           doc.save('download.pdf');
-        }
+        },
       });
     }
-    console.log(element)
+    console.log(element);
   }
-  print(item:any){
-    let  popupWin;
-		// printContents = document.getElementById('contentToExport')?.innerHTML;
-		popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+  print(item: any) {
+    let popupWin;
+    // printContents = document.getElementById('contentToExport')?.innerHTML;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
     // let prayer_request=item.prayer_request.replace(/\n/g, '<br>')
     // let testimony=item.testimony.replace(/\n/g, '<br>')
-    let prayer_request=item.prayer_request.trimLeft()
-    let testimony=item.testimony.trimLeft()
-		if(popupWin){
-			popupWin.document.open();
-			popupWin.document.write(`
+    let prayer_request = item.prayer_request.trimLeft();
+    let testimony = item.testimony.trimLeft();
+    if (popupWin) {
+      popupWin.document.open();
+      popupWin.document.write(`
 				<html>
 					<head>
 						<title>${item.name}</title>
@@ -408,31 +387,31 @@ ngAfterViewInit(): void {
                 <div  style="width:100%">
                   Obstacles in the Ministry :
                <div style="width: 100%;
-  white-space: break-spaces;
-  padding:5px;
-  font-family: '${item.obstacle_font}', sans-serif;">
-  ${item.obstacles}
-</div>
+                  white-space: break-spaces;
+                  padding:5px;font-size:28px;
+                  font-family: '${item.obstacle_font}', sans-serif;">
+                  ${item.obstacles}
+                </div>
                 <div  style="width:100%;">
                  Testimony :
                 </div>
                 
                <div style="width: 100%;
-  white-space: break-spaces;
-  padding:5px;
-  font-family: '${item.testimony_font}', sans-serif;">
-  ${testimony}
-</div>
+                  white-space: break-spaces;
+                  padding:5px;font-size:28px;
+                  font-family: '${item.testimony_font}', sans-serif;">
+                  ${testimony}
+                </div>
                 <div  style="width:100%;">
                  Prayer Request :
                 </div>
                 
               <div style="width: 90%;
-  white-space: pre-wrap;
-  padding:5px;
-  font-family: '${item.prayer_request_font}', sans-serif;">
-  ${prayer_request}
-</div>
+                white-space: pre-wrap;
+                padding:5px;font-size:28px;
+                font-family: '${item.prayer_request_font}', sans-serif;">
+                ${prayer_request}
+              </div>
 
               </div>
               
@@ -442,7 +421,7 @@ ngAfterViewInit(): void {
             </body>
           </head>
         </html>
-      `)
+      `);
       // popupWin.document.close();
     }
   }
@@ -456,16 +435,15 @@ ngAfterViewInit(): void {
           this.service.set('full_name', res.data.full_name);
           this.service.set('token', res.data.token);
           this.service.set('user_id', res.data.user_id);
-          let array=res.data.module_access.split(',')
-          if(!(array.includes('3'))){
+          let array = res.data.module_access.split(',');
+          if (!array.includes('3')) {
             this.service.deleteAll();
             localStorage.clear();
             sessionStorage.clear();
             this.token = '';
-            this.navbar.ngOnInit()
-            this.router.navigate(['/home'])
+            this.navbar.ngOnInit();
+            this.router.navigate(['/home']);
             // window.location.replace('/home')
-
           }
         } else {
           this.service.deleteAll();
@@ -475,10 +453,9 @@ ngAfterViewInit(): void {
           // window.location.reload()
         }
       });
-    this.get_year()
-    this.get_employees()
-    this.get_field_report()
-
+      this.get_year();
+      this.get_employees();
+      this.get_field_report();
     }
   }
 }
